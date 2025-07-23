00409f0edb5a4c43960f3ac50a75359fb401ab0e
Anton <dedul.9@yandex.ru>
2025-07-23 09:46:33 +0300

    Added header mockup

--- translations/en/translation.js
+++ translations/en/translation.js
@@ -1,16 +1,7 @@
 export default {
   header: {
-    title: 'CV - Portfolio',
-    language: 'Language',
-  },
-  about: {
-    title: 'About Me',
-    description:
-      'Frontend Developer with experience in modern web technologies',
-  },
-  contacts: {
-    title: 'Contacts',
-    email: 'Email',
-    phone: 'Phone',
+    name: 'Anton Chernookiy',
+    resume: 'Resume',
+    position: 'Frontend Developer',
   },
 };
--- translations/ru/translation.js
+++ translations/ru/translation.js
@@ -1,16 +1,7 @@
 export default {
   header: {
-    title: 'Резюме - Портфолио',
-    language: 'Язык',
-  },
-  about: {
-    title: 'Обо мне',
-    description:
-      'Frontend-разработчик с опытом работы в современных веб-технологиях',
-  },
-  contacts: {
-    title: 'Контакты',
-    email: 'Электронная почта',
-    phone: 'Телефон',
+    name: 'Антон Черноокий',
+    resume: 'Резюме',
+    position: 'Frontend-разработчик',
   },
 };
