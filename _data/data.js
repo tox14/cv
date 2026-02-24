e92eba478cceb4cb9fce0889bff0a58a26bb3031
Anton <dedul.9@yandex.ru>
2026-02-24 22:46:59 +0300

    Add base CV layout and data file

--- _data/data.js
+++ _data/data.js
@@ -4,19 +4,11 @@ export default {
     resume: 'Resume',
     name: 'Anton Chernookiy',
     position: 'Frontend Developer',
-    lang: {
-      switchLabel: 'RU',
-      switchTo: 'ru',
-    },
   },
   ru: {
     title: 'CV - Портфолио',
     resume: 'Резюме',
     name: 'Антон Черноокий',
     position: 'Frontend-разработчик',
-    lang: {
-      switchLabel: 'EN',
-      switchTo: 'en',
-    },
   },
 };
