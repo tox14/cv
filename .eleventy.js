d5aef444d5d64d2827a16f239f4fdb53e23d6d80
Anton <dedul.9@yandex.ru>
2026-02-24 21:58:28 +0300

    Refactoring eleventy architecture

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,28 @@
 import htmlmin from 'html-minifier-terser';
+import { readFileSync } from 'node:fs';
 
 export default function (eleventyConfig) {
   eleventyConfig.addTransform('htmlmin', function (content) {
     if (this.outputPath && this.outputPath.endsWith('.html')) {
+      if (content.includes('<link rel="stylesheet" href="styles.css" />')) {
+        const cssContent = readFileSync('styles.css', 'utf-8');
+        content = content.replace(
+          '<link rel="stylesheet" href="styles.css" />',
+          `<style>\n${cssContent}\n    </style>`
+        );
+      }
       return htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
+        minifyCSS: true,
       });
     }
     return content;
   });
 
   eleventyConfig.addPassthroughCopy('translations');
+  eleventyConfig.addPassthroughCopy('js');
 
   return {
     dir: {
