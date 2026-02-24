40b34437fcbebd50db12ac88b4adc8b827d7ae0c
Anton <dedul.9@yandex.ru>
2026-02-24 22:38:40 +0300

    Rename translations to data to simplify

---
+++ _data/data.js
@@ -0,0 +1,22 @@
+export default {
+  en: {
+    title: 'CV - Portfolio',
+    resume: 'Resume',
+    name: 'Anton Chernookiy',
+    position: 'Frontend Developer',
+    lang: {
+      switchLabel: 'RU',
+      switchTo: 'ru',
+    },
+  },
+  ru: {
+    title: 'CV - Портфолио',
+    resume: 'Резюме',
+    name: 'Антон Черноокий',
+    position: 'Frontend-разработчик',
+    lang: {
+      switchLabel: 'EN',
+      switchTo: 'en',
+    },
+  },
+};
