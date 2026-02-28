b47ac775c7b6c5c9a253e59d7d0f771af5295851
Anton <dedul.9@yandex.ru>
2026-02-28 21:11:45 +0300

    Minor css fixes

--- _data/data.js
+++ _data/data.js
@@ -4,11 +4,13 @@ export default {
     resume: 'Resume',
     name: 'Anton Chernookiy',
     position: 'Frontend Developer',
+    download_pdf: 'Download PDF',
   },
   ru: {
     title: 'CV - Портфолио',
     resume: 'Резюме',
     name: 'Антон Черноокий',
     position: 'Frontend-разработчик',
+    download_pdf: 'Скачать PDF',
   },
 };
